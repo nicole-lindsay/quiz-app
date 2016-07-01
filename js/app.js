14d3a26caf59e2824cb06b5298b8fbbd69b7f749
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-07-01 08:56:33 -0400

    submit button actually does something now

--- js/app.js
+++ js/app.js
@@ -1,40 +1,46 @@
-$(document).ready(function() {
-    var quiz = [{
-        question: "What is the definition of the word <b>fortnight</b>?",
-        options: ["A month", "A day", "Two weeks", "A week"],
-        correct: 2 // one single number in an array?? : [2]
-        // arrays are great if you have a collection
-    }, {
-        question: "Which word is synonymous with vitriol?",
-        options: ["angry words", "pleasantness", "obtuse", "pageantry"],
-        correct: 0
-    }, {
-        question: "What is the definition of perspicacious?",
-        options: ["intent", "arrogance", "shrewd", "dull"],
-        correct: 2
-    }, {
-        question: "What is the antonym of the word adversary?",
-        options: ["enemy", "alter ego", "fish", "friend"],
-        correct: 3
-    }, {
-        question: "What is the definition of the word poignant?",
-        options: ["moving", "bland", "wrong", "unaffecting"],
-        correct: 0
-    }];
-
-    $(".questions p").append(quiz[0].question);
-
-    for (var i = 0; i < quiz[0].options.length; i++) {
-    	// imposter syndrome
-        // i -> 0
-        // i -> 1
-        // i -> 2; 
-        // x = [1,2,3]
-        // x[i] ---> 1 or 2 or 3
-        $(".answers ul").append('<li><input type="radio" name="option"><span class="options">' + quiz[0].options[i] + '</span></li>');
+//a function to write question & answer on the page
+function writeQuestion(question) {
+    $(".questions p").html(question.text);
+    $(".answers ul").empty();
+    for (var i = 0; i < question.options.length; i++) {
+        $(".answers ul").append('<li><input type="radio" name="option"><span class="options">' + question.options[i] + '</span></li>');
     }
+}
+
+var quizList = [{
+    text: "What is the definition of the word <b>fortnight</b>?",
+    options: ["A month", "A day", "Two weeks", "A week"],
+    correct: 2 // one single number in an array?? : [2]
+    // arrays are great if you have a collection
+}, {
+    text: "Which word is synonymous with <b>vitriol</b>?",
+    options: ["angry words", "pleasantness", "obtuse", "pageantry"],
+    correct: 0
+}, {
+    text: "What is the definition of <b>perspicacious</b>?",
+    options: ["intent", "arrogance", "shrewd", "dull"],
+    correct: 2
+}, {
+    text: "What is the antonym of the word <b>adversary</b>?",
+    options: ["enemy", "alter ego", "fish", "friend"],
+    correct: 3
+}, {
+    text: "What is the definition of the word <b>poignant</b>?",
+    options: ["moving", "bland", "wrong", "unaffecting"],
+    correct: 0
+}];
 
 
-    // dynamic (1, 2, or 3);
-    // respond to the SUBMIT button clicks or form submission $('form').submit(e){ } or $(SOME kind of button).click(e) { e.preventDefault() }
+$(document).ready(function() {
+    var currentQuestion = 0
+
+    writeQuestion(quizList[currentQuestion])
+
+    //changes the Q&A when Submit is pressed
+    $("#submit").click(function(event) {
+        event.preventDefault();
+        currentQuestion++;
+        writeQuestion(quizList[currentQuestion]);
+    });
+
 });
